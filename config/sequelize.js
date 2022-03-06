02627ee8a109d9c29b661505574baefe6b1b5fd3
baimempatlima <baimempatlima@gmail.com>
2022-03-06 10:35:19 +0700

    update

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,24 @@
 const { Sequelize } = require("sequelize");
 
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const sequelize = new Sequelize({
-  database: "latihan-cruds-v2",
-  host: "localhost",
-  username: "root",
-  password: "root",
-  dialect: "mysql",
+  database: "d4pvd1o5hb03rj",
+  host: "ec2-3-225-79-57.compute-1.amazonaws.com",
+  username: "yddmbjdostxuyy",
+  password: "3df6e598424afebc86b548ac8d8f7bca5c6a55a8fd988674ae0099f72b39b29c",
+  port: "5432",
+  dialect: "postgres",
 });
+
+// postgres://yddmbjdostxuyy:3df6e598424afebc86b548ac8d8f7bca5c6a55a8fd988674ae0099f72b39b29c@ec2-3-225-79-57.compute-1.amazonaws.com:5432/d4pvd1o5hb03rj
+// const sequelize = new Sequelize({
+//   database: "latihan-cruds-v2",
+//   host: "localhost",
+//   username: "root",
+//   password: "root",
+//   dialect: "mysql",
+// });
 
 (async () => {
   try {
