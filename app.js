fa619d890791de312ddf1d06755999dbb69687c1
baimempatlima <baimempatlima@gmail.com>
2022-03-11 10:02:52 +0700

    update task express-mysql-sequelize

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+// const productRouter = require("./app/product/routes");
 const productRouterV2 = require("./app/product_v2/routes");
 const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
-// require("dotenv").config();
-
-// const corsOption = { origin: process.env.URL || "*" };
 
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
+// app.use("/api/v1", productRouter);
 app.use("/api/v2", productRouterV2);
 
 app.use("/", (req, res, next) => {
@@ -24,4 +23,4 @@ app.use("/", (req, res, next) => {
   });
 });
 
-app.listen(PORT, () => console.log(`Server: ${PORT} `));
+app.listen(PORT, () => console.log(`Server: http://localhost:${PORT} `));
--- config/sequelize.js
+++ config/sequelize.js
@@ -1,39 +1,12 @@
 const { Sequelize } = require("sequelize");
-// require("dotenv").config();
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-// });
-
-// const sequelize = new Sequelize("latihan-cruds-v2", "postgres", "root", {
-//   host: "localhost",
-//   port: "5432",
-//   dialect: "postgres",
-// });
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: 0,
-  },
-  dialect: "postgres",
+const sequelize = new Sequelize({
+  database: "latihan-cruds-v2",
+  host: "localhost",
+  username: "root",
+  password: "root",
+  dialect: "mysql",
 });
-// (
-// const sequelizeConfig = process.env.DATABASE_URL
-//   ? {
-//       connectionString: process.env.DATABASE_URL,
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     }
-//   : {
-//       database: "latihan-cruds-v2",
-//       host: "localhost",
-//       username: "postgres",
-//       password: "root",
-//       dialect: "postgres",
-//     };
-// const sequelize = new Sequelize(sequelizeConfig);
 
 (async () => {
   try {
@@ -45,44 +18,3 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 })();
 
 module.exports = sequelize;
-
-// const sequelize = new Sequelize({
-//   database: "latihan-cruds-v1",
-//   host: "localhost",
-//   username: "postgres",
-//   password: "root",
-//   port: "5432",
-//   dialect: "postgres",
-// });
-
-// database: "d4pvd1o5hb03rj",
-// host: "ec2-3-225-79-57.compute-1.amazonaws.com",
-// username: "yddmbjdostxuyy",
-// password: "3df6e598424afebc86b548ac8d8f7bca5c6a55a8fd988674ae0099f72b39b29c",
-// port: "5432",
-// dialect: "postgres",
-
-// database: "d711mo9skr755g",
-// host: "ec2-54-83-21-198.compute-1.amazonaws.com",
-// username: "nyvphvxmjadrck",
-// password: "8ea98f708059d21ce57adb8c48fd6a8c43c38679bea223578d820d513cb35105",
-// port: "5432",
-// dialect: "postgres",
-
-// const sequelize = new Sequelize({
-//   database: "d4pvd1o5hb03rj",
-//   host: "ec2-3-225-79-57.compute-1.amazonaws.com",
-//   username: "yddmbjdostxuyy",
-//   password: "3df6e598424afebc86b548ac8d8f7bca5c6a55a8fd988674ae0099f72b39b29c",
-//   port: "5432",
-//   dialect: "postgres",
-// });
-
-// postgres://yddmbjdostxuyy:3df6e598424afebc86b548ac8d8f7bca5c6a55a8fd988674ae0099f72b39b29c@ec2-3-225-79-57.compute-1.amazonaws.com:5432/d4pvd1o5hb03rj
-// const sequelize = new Sequelize({
-//   database: "latihan-cruds-v2",
-//   host: "localhost",
-//   username: "root",
-//   password: "root",
-//   dialect: "mysql",
-// });
