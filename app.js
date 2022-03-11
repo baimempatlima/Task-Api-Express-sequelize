88da9c1f36f6572a14ae1ac63685d21d2757e01b
baimempatlima <baimempatlima@gmail.com>
2022-03-11 08:30:16 +0700

    update task

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use("/api/v2", productRouterV2);
 
-app.use((req, res, next) => {
-  res.status(404);
+app.use("/", (req, res, next) => {
+  res.status(200);
   res.send({
-    status: "failed",
-    message: "Resource: " + req.originalUrl + " Not Found",
+    status: "success",
+    message: "Welcome to express api sequelize",
   });
 });
 
--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,13 @@
 const { Sequelize } = require("sequelize");
 // require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "postgres",
+// });
+
+const sequelize = new Sequelize("latihan-cruds-v2", "postgres", "root", {
+  host: "localhost",
+  port: "5432",
   dialect: "postgres",
 });
 // (
