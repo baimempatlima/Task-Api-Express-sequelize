081c434f9a2e819e702a463258cacda29898d674
baimempatlima <baimempatlima@gmail.com>
2022-03-13 20:58:21 +0700

    update

--- config/sequelize.js
+++ config/sequelize.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize({
   host: process.env.DB_HOST || "localhost",
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "root",
-  port: process.env.DB_PORT || 3000,
+  port: process.env.DB_PORT || 3306,
   dialect: "postgres" || "mysql",
 });
 
